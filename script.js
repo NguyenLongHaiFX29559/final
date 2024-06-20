e582911d396dbb9b4a14c8d2d4e3b3733581197b
hainlfx29559 <hainlfx29559@funix.edu.vn>
2024-06-20 18:01:04 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+document.querySelector("button").addEventListener("click", function (event) {
+  event.preventDefault();
+  const correctEmail = "hainlFX29559@funix.edu.vn";
+  const enteredEmail = document.getElementById("email").value; // Use value instead of textContent
+  if (enteredEmail === correctEmail) {
+    document.querySelector("#personal-info").style.display = "flex";
+    document.querySelector("#personal-info-email").style.display = "none";
+    console.log("done");
+  } else {
+    console.log("kill");
+  }
+});
+
+const mainInfo = document.querySelectorAll(".main-info");
+
+for (let i = 0; i < mainInfo.length; i++) {
+  mainInfo[i].addEventListener("mouseover", function () {
+    if (
+      !this.querySelector(".job-history").style.display ||
+      this.querySelector(".job-history").style.display === "none"
+    ) {
+      this.querySelector(".show-more-btn").style.display = "flex";
+    } else {
+      this.querySelector(".show-less-btn").style.display = "flex";
+    }
+  });
+
+  mainInfo[i].addEventListener("mouseout", function () {
+    this.querySelector(".show-more-btn").style.display = "none";
+    this.querySelector(".show-less-btn").style.display = "none";
+  });
+
+  mainInfo[i]
+    .querySelector(".show-more-btn")
+    .addEventListener("click", function () {
+      console.log("show more button clicked!");
+      const parent = this.closest(".main-info");
+      parent.querySelector(".job-history").style.display = "grid";
+      parent.querySelector(".info-header").style.height = "13rem";
+      parent.style.height = "64rem";
+
+      this.style.display = "none";
+      parent.querySelector(".show-less-btn").style.display = "flex";
+    });
+
+  mainInfo[i]
+    .querySelector(".show-less-btn")
+    .addEventListener("click", function () {
+      console.log("show less button clicked!");
+      const parent = this.closest(".main-info");
+      parent.querySelector(".job-history").style.display = "none";
+      parent.querySelector(".info-header").style.height = "13rem";
+      parent.style.height = "20rem";
+
+      this.style.display = "none";
+      parent.querySelector(".show-more-btn").style.display = "flex";
+    });
+}
